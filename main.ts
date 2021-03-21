e22fb3c5a8458e280c1f4e0ff6dfe3a02143cb25
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-03-21 19:09:33 +1300

    Changed event to from Forever to Button A

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, randint(0, 180))
     basic.pause(200)
     pins.servoWritePin(AnalogPin.P1, randint(0, 180))
